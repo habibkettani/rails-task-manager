8e199443eb5af785c650e220248843ee869c15af
Habib Kettani <habibkettani03@gmail.com>
2026-02-24 14:29:41 +0100

    Task Manager DONE

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,17 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "tasks", to: "tasks#index"
+
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+
+
+  get "tasks/:id", to: "tasks#show", as: :task
+  delete "tasks/:id", to: "tasks#destroy"
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
